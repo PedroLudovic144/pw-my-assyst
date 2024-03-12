cc15c142798622ce9312673eac8912ddc1010b63
PedroLudovic144 <pedroludovic5@gmail.com>
2024-03-12 08:31:14 -0300

    Commit folders and files

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,4 @@
+/*Pedro Ludovic rm:231742 - Cassia Freitas Molinaro rm:231103  2°ai */
 public class Cliente {
 
     int cpf;
---
+++ src/TipoDeEquipamneto.java
@@ -0,0 +1,6 @@
+public enum TipoDeEquipamneto {
+    Celular,
+    Tablet,
+    Computador;
+
+}
